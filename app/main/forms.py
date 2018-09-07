b55703dc23e4faac4f9d8e4f95dfbd4da06a0ba4
danalvin <mungaiwaituika@gmail.com>
2018-09-07 22:49:21 +0300

    debugging

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,52 @@
-from flask import Blueprint
-main = Blueprint('main',__name__)
-from . import views,errors
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField, StringField
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class PromotionForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class PickForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class ProductionForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class InterviewForm(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class PromotionCommentForm(FlaskForm):
+    comment = StringField('Comment: ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class PickCommentForm(FlaskForm):
+    comment = StringField('Comment:', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class ProductionCommentForm(FlaskForm):
+    comment = StringField('Comment: ', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class InterviewCommentForm(FlaskForm):
+    comment = StringField('Comment: ', validators=[Required()])
+    submit = SubmitField('Submit')
